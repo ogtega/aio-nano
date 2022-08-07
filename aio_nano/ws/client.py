5374f2d8ab77f4427fed8feffefc31915789e975
Teslim Olunlade <tolunlade@outlook.com>
2022-08-07 14:04:45 -0400

    Added message callbacks and parsing

--- aio_nano/ws/client.py
+++ aio_nano/ws/client.py
@@ -7,6 +7,16 @@ from websockets.client import WebSocketClientProtocol, connect
 from websockets.exceptions import ConnectionClosed
 from websockets.typing import Data
 
+from aio_nano.ws.models import (
+    Block,
+    Bootstrap,
+    Confirmation,
+    Difficulty,
+    PoW,
+    Telemetry,
+    Vote,
+)
+
 
 class Topic(str, Enum):
     confirmation = "confirmation"
@@ -27,6 +37,17 @@ class WSClient(connect):
     loop: asyncio.AbstractEventLoop
     _callbacks: dict[str, list[Callable]]
 
+    _parsers: dict[str, Callable[[dict], object]] = {
+        "confirmation": lambda msg: Confirmation.parse_obj(msg),
+        "vote": lambda msg: Vote.parse_obj(msg),
+        "stopped_election": lambda msg: msg.get("hash"),
+        "active_difficulty": lambda msg: Difficulty.parse_obj(msg),
+        "work": lambda msg: PoW.parse_obj(msg),
+        "telemetry": lambda msg: Telemetry.parse_obj(msg),
+        "new_unconfirmed_block": lambda msg: Block.parse_obj(msg),
+        "bootstrap": lambda msg: Bootstrap.parse_obj(msg),
+    }
+
     def __init__(self, uri: str, **args) -> None:
         self.connection = connect(uri, **args)
         self.loop = asyncio.get_event_loop()
@@ -42,7 +63,17 @@ class WSClient(connect):
     async def _recv(self):
         while True:
             try:
-                print(await self.client.recv())
+                data = json.loads(await self.client.recv())
+                topic = data.get("topic") or ""
+                msg = data.get("message") or {}
+
+                for cb in self._callbacks[topic]:
+                    obj = self._parsers.get(topic, lambda _: None)(msg)
+                    if asyncio.iscoroutinefunction(cb):
+                        asyncio.create_task(cb(obj))
+                        continue
+                    cb(obj)
+
             except ConnectionClosed:
                 self.client = await self.connection
 
@@ -50,22 +81,7 @@ class WSClient(connect):
     async def subscribe(
         self,
         topic: Literal[Topic.confirmation] | Literal["confirmation"],
-        cb: Callable[[object], Any],
-        **options: Any,
-    ):
-        ...
-
-    @overload
-    async def subscribe(
-        self, topic: Literal[Topic.vote], cb: Callable[[object], Any], **options: Any
-    ):
-        ...
-
-    @overload
-    async def subscribe(
-        self,
-        topic: Literal[Topic.stopped_election],
-        cb: Callable[[object], Any],
+        cb: Callable[[Confirmation], Any],
         **options: Any,
     ):
         ...
@@ -73,23 +89,8 @@ class WSClient(connect):
     @overload
     async def subscribe(
         self,
-        topic: Literal[Topic.active_difficulty],
-        cb: Callable[[object], Any],
-        **options: Any,
-    ):
-        ...
-
-    @overload
-    async def subscribe(
-        self, topic: Literal[Topic.pow], cb: Callable[[object], Any], **options: Any
-    ):
-        ...
-
-    @overload
-    async def subscribe(
-        self,
-        topic: Literal[Topic.telemetry],
-        cb: Callable[[object], Any],
+        topic: Literal[Topic.vote] | Literal["vote"],
+        cb: Callable[[Vote], Any],
         **options: Any,
     ):
         ...
@@ -97,8 +98,8 @@ class WSClient(connect):
     @overload
     async def subscribe(
         self,
-        topic: Literal[Topic.new_unconfirmed_block],
-        cb: Callable[[object], Any],
+        topic: Literal[Topic.stopped_election] | Literal["stopped_election"],
+        cb: Callable[[str], Any],
         **options: Any,
     ):
         ...
@@ -106,14 +107,50 @@ class WSClient(connect):
     @overload
     async def subscribe(
         self,
-        topic: Literal[Topic.bootstrap],
-        cb: Callable[[object], Any],
+        topic: Literal[Topic.active_difficulty] | Literal["active_difficulty"],
+        cb: Callable[[Difficulty], Any],
+        **options: Any,
+    ):
+        ...
+
+    @overload
+    async def subscribe(
+        self,
+        topic: Literal[Topic.pow] | Literal["work"],
+        cb: Callable[[PoW], Any],
+        **options: Any,
+    ):
+        ...
+
+    @overload
+    async def subscribe(
+        self,
+        topic: Literal[Topic.telemetry] | Literal["telemetry"],
+        cb: Callable[[Telemetry], Any],
+        **options: Any,
+    ):
+        ...
+
+    @overload
+    async def subscribe(
+        self,
+        topic: Literal[Topic.new_unconfirmed_block] | Literal["new_unconfirmed_block"],
+        cb: Callable[[Block], Any],
+        **options: Any,
+    ):
+        ...
+
+    @overload
+    async def subscribe(
+        self,
+        topic: Literal[Topic.bootstrap] | Literal["bootstrap"],
+        cb: Callable[[Bootstrap], Any],
         **options: Any,
     ):
         ...
 
     async def subscribe(
-        self, topic: Topic | str, cb: Callable[[object], Any], **options: Any
+        self, topic: Topic | str, cb: Callable[[Any], Any], **options: Any
     ):
         await self.send(
             json.dumps(
